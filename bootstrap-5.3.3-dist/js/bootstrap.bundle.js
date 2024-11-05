d99788179b37f4341d7e41b8c41a637531a27dc6
Safar1973 <fsafarm1@gmail.com>
2024-11-05 10:29:12 +0100

    Willkommen auf meiner persönlischen websete

--- bootstrap-5.3.3-dist/js/bootstrap.bundle.js
+++ bootstrap-5.3.3-dist/js/bootstrap.bundle.js
@@ -6309,6 +6309,79 @@
   };
 
   return index_umd;
+  <!DOCTYPE html>
+  <html lang="de">
+<head> 
+  <br>
+    <meta charset="UTF-8">
+    <title>Faiek Safar</title>
+    <!--IMPORTING BOOTSTRAP-->
+   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+   <link href="css/styles.css" rel="stylesheet">
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="index.html">Meine Website</a>
+  
+  <div class="collapse navbar-collapse">
+      
+     
+  <ul class="navbar-nav mr-auto">
+        
+  <li class="nav-item"><a class="nav-link" href="index.html">Startseite</a></li>
+    
+       
+  <li class="nav-item"><a class="nav-link" href="about.html">Über mich</a></li>
+        
+     
+  <li class="nav-item"><a class="nav-link" href="login.html">Login</a></li>
+      </ul>
+    
+   
+  </div>
+  </nav>
+    <div class="container">
+    <h1>Willkommen auf meiner Persönlichen Website!</h1>
+    <br>
+    <br>
+    <p>Kurs: Web Programmierung by Ali Khorsandfard</p>
+    <br>
+    <p>Informatiker Winter 2024</p>
+    <br>
+    <p>Name: Faiek Safar</p>
+    <br>
+    <p>Standort: Willy-Brandt-Platz3 46045 Oberhausen</p>
+    
+    
+    <form>
+        <div class="form-group">
+          <br>
+          <label for="email">Email:</label>
+          <br>
+          <input type="email" class="form-control" id="email" placeholder="Enter email">
+        
+        </div>
+        <div class="form-group">
+          <br>
+          
+         <label for="message">Nachricht:</label>
+         <textarea class="form-control" id="message" rows="3" placeholder="Enter your message"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Kontaktiere mich</button>
+
+    </form>
+  
+       </div> 
+       <br> 
+       <br> 
+       <footer class="footer bg-light text-center py-3">
+        <p>&copy; 2024 von [Faiek Safar]</p>
+       </footer>
+       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+       
+</body>  
+</html>
 
 }));
 //# sourceMappingURL=bootstrap.bundle.js.map
